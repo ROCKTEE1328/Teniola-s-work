cafc0f21cfdf72ea67887f0f762fe618b13369c3
unknown <rojomoyela@gmail.com>
2023-10-16 18:05:04 -0700

    Girl this was hard

---
+++ index.js
@@ -0,0 +1,60 @@
+//this is a calculate to add two numbers
+let x = 1000
+let y= 1000
+
+let total = x+y
+console.log(total)
+
+//this is a calculate to subtract two numbers
+let r = 10
+let a = 9
+let z = r-a
+
+console.log(z)
+
+//calculation of area of a reactangle
+let lenght = 50
+let breadth = 10
+const area = lenght*breadth
+console.log("area of reactangle =" + area)
+
+//calculation of area of a triangle
+let width = 30
+let hight = 80
+let q =width*hight
+const value = q/2
+console.log("area of triangle=" + value)
+
+
+//calculation of circumstance of a circle
+let radius = 10;
+let circumference = 5 * Math.PI * radius;
+console.log("The circumference of the circle is " + circumference);
+
+//This function adds two numbers
+function addition(s,v){
+    let add = s+v
+    return add
+    
+}
+console.log(addition(100,200))
+
+//This function multiply two numbers
+function multiplication(u,r){
+    let multipy = u*r
+    return multipy
+    
+}
+console.log( multiplication(10,201))
+
+//This function subtraction two numbers
+function subtraction (a,c){
+    let subtract = a-c
+    return subtract 
+    
+}
+console.log(subtraction(7,220))
+
+
+
+
